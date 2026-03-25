015affe364441fd803f149beadf3e346aa618c0c
Ramon Martinez <martinezcjr1@gmail.com>
2026-03-24 22:23:15 -0700

    feat: feat(network): configurar instancia global de axios con interceptores base y establecer arquetipo fdd

---
+++ src/config/axios.ts
@@ -0,0 +1,44 @@
+// src/config/axios.ts
+import axios from 'axios';
+
+// 1. Forjamos la instancia base apuntando a nuestro galeón FastAPI
+const apiClient = axios.create({
+  baseURL: 'http://localhost:8000/api', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  // Nota de seguridad: conCredentials en true es necesario si usamos cookies, 
+  // pero como usaremos JWT en el header 'Authorization', lo omitimos por ahora.
+});
+
+// 2. Aduana de Salida (Interceptores de Petición)
+apiClient.interceptors.request.use(
+  (config) => {
+    // TODO (Fase Auth): Recuperar el token del almacenamiento seguro.
+    // const token = localStorage.getItem('delonix_jwt');
+    // if (token && config.headers) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 3. Aduana de Entrada (Interceptores de Respuesta)
+apiClient.interceptors.response.use(
+  (response) => {
+    // Si la petición fue exitosa (200-299), dejamos pasar los datos intactos
+    return response;
+  },
+  (error) => {
+    // TODO (Fase Auth): Capturar errores 401 para hacer un "logout" forzoso
+    // if (error.response?.status === 401) {
+    //    window.location.href = '/login';
+    // }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
